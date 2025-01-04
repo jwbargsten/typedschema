b0564324243bf4e6883319eb6d00e777145ae347
Joachim Bargsten <jw@bargsten.org>
2025-01-05 00:09:23 +0100

    Improved README and added examples

--- src/typedschema/__init__.py
+++ src/typedschema/__init__.py
@@ -1,4 +1,4 @@
 from typedschema.column import Column
-from typedschema.schema import Schema
+from typedschema.schema import Schema, diff_schemas
 
-__all__ = ["Column", "Schema"]
+__all__ = ["Column", "Schema", "diff_schemas"]
